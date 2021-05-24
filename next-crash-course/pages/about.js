1520d85b587753830083555f1897d01209bb98c7
99lalo <74028591+99lalo@users.noreply.github.com>
2021-05-23 21:24:58 -0400

    About

--- next-crash-course/pages/about.js
+++ next-crash-course/pages/about.js
@@ -1,9 +1,17 @@
 import Meta from "../components/Meta";
+import Link from "next/link";
+import aboutStyles from "../styles/About.module.css";
 const about = () => {
   return (
-    <div>
+    <div className={aboutStyles.about}>
       <Meta title="About" />
       <h1>About</h1>
+      <p>
+        This is a website made following Next.js Crash Course from{" "}
+        <Link href="https://www.youtube.com/channel/UC29ju8bIPH5as8OGnQzwJyA">
+          Traversy Media
+        </Link>
+      </p>
     </div>
   );
 };
